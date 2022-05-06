1adc1f54e3f2d8333f850e8d31479d25571ec3b8
freddykaberuka <freddykaberuka2009@gmail.com>
2022-05-06 12:06:03 +0200

    add linters

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
 import './style.css';
+
 const todoForm = document.querySelector('#todo-form');
 const todoList = document.querySelector('.list-todos');
 
 class Todo {
-    constructor(description, id, completed) {
-        this.description = description;
-        this.id = id;
-        this.completed = completed;
-    }
+  constructor(description, id, completed) {
+    this.description = description;
+    this.id = id;
+    this.completed = completed;
+  }
 }
 
 let todos = [];
 
 // display todos
 const displayTodos = () => {
-    const data = localStorage.getItem('todostorage');
-    if(data === undefined || data === null){ return;}
-    
-        const parsedData = JSON.parse(data);
-        todos = parsedData;
-        todoList.innerHTML = parsedData.map((el) => 
-            `<div>
+  const data = localStorage.getItem('todostorage');
+  if (data === undefined || data === null) { return; }
+
+  const parsedData = JSON.parse(data);
+  todos = parsedData;
+  todoList.innerHTML = parsedData.map((el) => `<div>
                         <div class="todo-item">
                             <div class="item-wrap"><input type="checkbox" class="checkbox">
                             <p>${el.description}</p></div>
                             <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
                         </div>   
-            </div>`
-        ).join('');
-}
+            </div>`).join('');
+};
 
 // add todo
 
-todoForm.addEventListener('submit',(e) => {
-    e.preventDefault();
-    const todoDescription = document.getElementById('description').value;
-    const id = todos.length;
-    const todo = new Todo(todoDescription, id, false);
-    todos = [...todos, todo];
-    localStorage.setItem('todostorage', JSON.stringify(todos));
-    document.getElementById('description').value = '';
-    displayTodos();
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todoDescription = document.getElementById('description').value;
+  const id = todos.length;
+  const todo = new Todo(todoDescription, id, false);
+  todos = [...todos, todo];
+  localStorage.setItem('todostorage', JSON.stringify(todos));
+  document.getElementById('description').value = '';
+  displayTodos();
 });
 
 window.onload(displayTodos());
