63faae0de8a031d22e94aed7ab8c5cc95e93e236
freddykaberuka <freddykaberuka2009@gmail.com>
2022-05-07 18:26:20 +0200

    implemet CRUD

--- src/index.js
+++ src/index.js
@@ -1,45 +1,26 @@
 import './style.css';
+import Todo from './modules/todo.js';
 
-const todoForm = document.querySelector('#todo-form');
-const todoList = document.querySelector('.list-todos');
+const todo = new Todo();
 
-class Todo {
-  constructor(description, id, completed) {
-    this.description = description;
-    this.id = id;
-    this.completed = completed;
-  }
-}
+todo.newTodolist();
+todo.displayTodo();
 
-let todos = [];
-
-// display todos
-const displayTodos = () => {
-  const data = localStorage.getItem('todostorage');
-  if (data === undefined || data === null) { return; }
-
-  const parsedData = JSON.parse(data);
-  todos = parsedData;
-  todoList.innerHTML = parsedData.map((el) => `<div>
-                        <div class="todo-item">
-                            <div class="item-wrap"><input type="checkbox" class="checkbox">
-                            <p>${el.description}</p></div>
-                            <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
-                        </div>   
-            </div>`).join('');
-};
-
-// add todo
-
-todoForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const todoDescription = document.getElementById('description').value;
-  const id = todos.length;
-  const todo = new Todo(todoDescription, id, false);
-  todos = [...todos, todo];
-  localStorage.setItem('todostorage', JSON.stringify(todos));
-  document.getElementById('description').value = '';
-  displayTodos();
+const editValue = document.querySelectorAll('.editInput');
+editValue.forEach((item) => {
+  item.addEventListener('change', (e) => {
+    todo.editTodo(e.target.value, e.target.dataset.id);
+  });
+  item.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      todo.editTodo(e.target.value, e.target.dataset.id);
+    }
+  });
 });
 
-window.onload(displayTodos());
+const trash = document.querySelectorAll('.delete');
+trash.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    todo.removeTodo(e.target.dataset.id);
+  });
+});
